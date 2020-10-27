fba24828e0a1f48a264418745746a8ff3dbff441
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-27 13:55:15 +0300

    create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <h2 className="header__title">NewsExplorer</h2>
+        </header>
+    )
+}
+
+export default Header;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Main.css'
+import Header from '../Header/Header';
 
 function Main() {
     return (
         <div className="main">
+            <Header />
             <form className="main__form">
                 <h1 className="main__title">Что творится в мире?</h1>
                 <p className="main__text">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
