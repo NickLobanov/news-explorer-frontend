9c8bc472a2399868a76fcecc34bef6f84fc61ca6
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-27 13:37:01 +0300

    add style main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Main.css'
 
 function Main() {
     return (
         <div className="main">
-            <h1 className="main__title">Что творится в мире?</h1>
-            <p className="main__text">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
             <form className="main__form">
-                <input className="main__input" type="text" placeholder="Введите тему новости"></input>
-                <button className="main__button">Искать</button>
+                <h1 className="main__title">Что творится в мире?</h1>
+                <p className="main__text">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+                <div className="main__wrap">
+                    <input className="main__input" type="text" placeholder="Введите тему новости"></input>
+                    <button className="main__button">Искать</button>
+                </div>
             </form>
         </div>
     )
