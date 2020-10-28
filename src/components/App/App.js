e7853479e0ac1f94bd7deef91d9bb3dc1554410f
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-28 15:07:28 +0300

    create footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from '../Main/Main';
 import About from '../About/About';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="page">
       <Main />
       <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__copyright">&#169; 2020 Supersite, Powered by News API</p>
+        </footer>
+    )
+}
+
+export default Footer;
