b59f4a37ac92bc1d40f9c3d017bb8ed64455565b
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-29 16:41:48 +0300

    create NewsCard component

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NewsCard.css';
+import cardImage from '../../images/card-image.png';
+
+function NewsCard() {
+    return (
+        <div className="news-card">
+            <img src={cardImage} alt="Изображение статьи" className="news-card__image"/>
+            <p className="news-card__date">2 августа, 2019</p>
+            <h3 className="news-card__title">Национальное достояние – парки</h3>
+            <p className="news-card__text">В 2016 году Америка отмечала важный юбилей:
+                сто лет назад здесь начала складываться система национальных парков – охраняемых территорий,
+                где и сегодня каждый может приобщиться к природе.</p>
+            <p className="news-card__source">Лента.ру</p>
+        </div>
+    )
+}
+
+export default NewsCard;
