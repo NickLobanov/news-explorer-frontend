8b5902140ee0021eccc6d9e18a14b075b61ff18c
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-01 15:10:03 +0300

    add style savedNewsHeader

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './SavedNews.css';
 import Header from '../Header/Header';
+import SavedNewsHeader from '../SavedNewsHeader/SaveNewsHeader';
 
 function SavedNews({isLogged}) {
     return (
-        <Header isLogged={isLogged}/>
+        <>
+            <Header isLogged={isLogged}/>
+            <SavedNewsHeader />
+        </>
     )
 }
 
