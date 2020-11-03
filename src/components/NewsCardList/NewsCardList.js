cfcc99fb775b5bf82dd228acdb4bc61d97093940
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-03 17:35:45 +0300

    add card list container

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -5,14 +5,16 @@ import NewsCard from '../NewsCard/NewsCard';
 function NewsCardList({ typeButton, hintText, showTitle, showButton }) {
     return (
         <section className="card-list">
-            {showTitle && <h2 className="card-list__title">Результаты поиска</h2>}
-            <div className="card-list__wrap">
-                <NewsCard typeButton={typeButton} hintText={hintText}/>
-                <NewsCard typeButton={typeButton} hintText={hintText}/>
-                <NewsCard typeButton={typeButton} hintText={hintText}/>
-                <NewsCard typeButton={typeButton} hintText={hintText}/>
+            <div className="card-list__container">
+                {showTitle && <h2 className="card-list__title">Результаты поиска</h2>}
+                <div className="card-list__wrap">
+                    <NewsCard typeButton={typeButton} hintText={hintText}/>
+                    <NewsCard typeButton={typeButton} hintText={hintText}/>
+                    <NewsCard typeButton={typeButton} hintText={hintText}/>
+                    <NewsCard typeButton={typeButton} hintText={hintText}/>
+                </div>
+                {showButton && <button type="button" className="card-list__button">Показать еще</button>}
             </div>
-            {showButton && <button type="button" className="card-list__button">Показать еще</button>}
         </section>
     )
 }
