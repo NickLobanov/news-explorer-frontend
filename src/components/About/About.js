9fe618aeac6a36260120ae41cca471198a219912
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-03 17:38:43 +0300

    add avatar container

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,16 +4,18 @@ import avatar from '../../images/avatar.jpg';
 
 function About() {
     return (
-        <div className="avatar">
-            <img src={avatar} alt="Аватар" className="avatar__image"/>
-            <div className="avatar__wrap">
-                <h2 className="avatar__title">Об авторе</h2>
-                <p className="avatar__text">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь,
-                     какими технологиями разработки владеете.</p>
-                <p className="avatar__text">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились,
-                     и чем можете помочь потенциальным заказчикам.</p>
+        <section className="avatar">
+            <div className="avatar__container">
+                <img src={avatar} alt="Аватар" className="avatar__image"/>
+                <div className="avatar__wrap">
+                    <h2 className="avatar__title">Об авторе</h2>
+                    <p className="avatar__text">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь,
+                        какими технологиями разработки владеете.</p>
+                    <p className="avatar__text">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились,
+                        и чем можете помочь потенциальным заказчикам.</p>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
