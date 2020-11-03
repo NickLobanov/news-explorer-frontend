a06fdf6e2495b86fdf7dc9b69c707071d6b0cf74
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-03 16:24:50 +0300

    add link Footer

--- src/components/NavigationFooter/NavigationFooter.js
+++ src/components/NavigationFooter/NavigationFooter.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import './NavigationFooter.css';
+import { Link } from 'react-router-dom';
 
 function NavigationFooter() {
     return(
         <ul className="nav">
-            <li className="nav__item nav__item_footer">Главная</li>
-            <li className="nav__item nav__item_footer">Яндекс.Практикум</li>
+            <li className="nav__item nav__item_footer"><Link to="/" className="nav__item-link">Главная</Link></li>
+            <li className="nav__item nav__item_footer"><a href="https://praktikum.yandex.ru/" className="nav__item-link">Яндекс.Практикум</a></li>
             <li className="nav__item nav__item_footer">
                 <div className="nav__wrap">
-                    <a className="nav__link nav__link_github"></a>
-                    <a className="nav__link nav__link_fb"></a>
+                    <a href="https://github.com/NickLobanov" className="nav__link nav__link_github"></a>
+                    <a href="https://ru-ru.facebook.com/" className="nav__link nav__link_fb"></a>
                 </div>
             </li>
         </ul>
