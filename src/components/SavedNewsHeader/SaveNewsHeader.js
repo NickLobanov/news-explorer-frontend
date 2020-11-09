3e7cd587636485e518d0a40536fea6465fd8e20b
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-09 16:14:51 +0300

    add responsive savedNewsHeader component

--- src/components/SavedNewsHeader/SaveNewsHeader.js
+++ src/components/SavedNewsHeader/SaveNewsHeader.js
@@ -4,14 +4,12 @@ import './SavedNewsHeader.css';
 function SavedNewsHeader() {
     return(
         <section className="news-info">
-            <div className="news-info__container">
-                <div className="news-info__wrap">
-                    <h3 className="news-info__title">Сохранённые статьи</h3>
-                    <p className="news-info__amout">Грета, у вас 5 сохранённых статей</p>
-                    <p className="news-info__keywords">По ключевым словам: <span className="news-info__keyword">Природа</span>,
-                        <span className="news-info__keyword">Тайга</span> и <span className="news-info__keyword">2-м другим</span>
-                    </p>
-                </div>
+            <div className="news-info__container">   
+                <h3 className="news-info__title">Сохранённые статьи</h3>
+                <p className="news-info__amount">Грета, у вас 5 сохранённых статей</p>
+                <p className="news-info__keywords">По ключевым словам: <span className="news-info__keyword">Природа</span>,
+                    <span className="news-info__keyword">Тайга</span> и <span className="news-info__keyword">2-м другим</span>
+                </p>   
             </div>
         </section>
     )
