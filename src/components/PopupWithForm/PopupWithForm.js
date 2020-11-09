2fb96c7e5c189e4fcd13b9005487ce619d370be4
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-09 21:41:29 +0300

    required input

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -12,10 +12,10 @@ function PopupWithForm({isSignUp, isOpen, isClose}) {
             <form className="popup__form">
                 <div className="popup__wrap">
                     <h3 className="popup__title">Вход</h3>
-                    <label className="popup__label">Email<input type="email" className="popup__input" placeholder="Введите почту"/></label>
-                    <label className="popup__label">Пароль<input type="password" className="popup__input" placeholder="Введите пароль"/></label>
+                    <label className="popup__label">Email<input type="email" className="popup__input" placeholder="Введите почту" required/></label>
+                    <label className="popup__label">Пароль<input type="password" className="popup__input" placeholder="Введите пароль" required/></label>
                     { isSignUp && 
-                        <label className="popup__label">Имя<input type="text" className="popup__input" placeholder="Введите свое имя"/></label>
+                        <label className="popup__label">Имя<input type="text" className="popup__input" placeholder="Введите свое имя" required/></label>
                     }
                     <button className="popup__button">Войти</button>
                     <p className="popup__text">или <span className="popup__link">Зарегистрироваться</span></p>
