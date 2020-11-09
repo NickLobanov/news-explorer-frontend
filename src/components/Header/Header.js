902418c36805416b9c23e25a2f305f402a25a5a6
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-09 14:52:42 +0300

    add style for width 660px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,13 @@ import Navigation from '../Navigation/Navigation';
 import { Link } from 'react-router-dom';
 
 function Header({ isLogged, darkType }) {
+
     return (
         <header className="header">
             <div className="header__container">
                 <Link to="/" className={`header__logo header__logo_${darkType}`}>NewsExplorer</Link>
                 <Navigation isLogged={isLogged} darkType={darkType}/>
+                <div className="header__burger-menu"></div>
             </div>
         </header>
     )
