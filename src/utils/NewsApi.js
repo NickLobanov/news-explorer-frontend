1e71ed586448898e3ead374c73a14a63d609ff4a
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-14 14:53:55 +0300

    create newsApi

---
+++ src/utils/NewsApi.js
@@ -0,0 +1,10 @@
+export const getNews = (keyword) => {
+    return fetch(`https://newsapi.org/v2/everything?q=${keyword}&pageSize=100&apiKey=e1e2ee9b439246f69224227bdc496b7c`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((res) => res.json())
+}
