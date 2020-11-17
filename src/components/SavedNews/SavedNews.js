2696ed8cb5f36f674f327f18e199a2afef1c1d79
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-17 21:05:20 +0300

    add currenUser to savedNews

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -3,12 +3,16 @@ import './SavedNews.css';
 import Header from '../Header/Header';
 import SavedNewsHeader from '../SavedNewsHeader/SaveNewsHeader';
 import NewsCardList from '../NewsCardList/NewsCardList';
+import { CurrentUserContext } from '../../contexts/currentUserContext';
 
 function SavedNews({isLogged, cardList}) {
+
+    const currentUser = React.useContext(CurrentUserContext)
+
     return (
         <>
-            <Header isLogged={isLogged} darkType="dark"/>
-            <SavedNewsHeader />
+            <Header isLogged={isLogged} darkType="dark" userName={currentUser.name}/>
+            <SavedNewsHeader userName={currentUser.name}/>
             <NewsCardList typeButton="delete"
                 hintText="Убрать из сохранённых"
                 showTitle={false}
