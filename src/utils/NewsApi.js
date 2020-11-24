6b0346e0b8fa959af7a5380a35742d837adce94a
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-24 17:29:52 +0300

    fix api

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,5 +1,5 @@
 export const getNews = (keyword, nowDay, prevDay) => {
-    return fetch(`https://newsapi.org/v2/everything?q=${keyword}&from=${prevDay}&to=${nowDay}pageSize=100&apiKey=e1e2ee9b439246f69224227bdc496b7c`, {
+    return fetch(`https://nomoreparties.co/news/v2/everything?q=${keyword}&from=${prevDay}&to=${nowDay}pageSize=100&apiKey=e1e2ee9b439246f69224227bdc496b7c`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
